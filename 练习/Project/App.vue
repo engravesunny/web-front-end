d36f164d75c95d0691378f7d979fb2e293506b00
engravesunny <3170557037@qq.com>
2023-10-14 20:39:55 +0800

    10.14

---
+++ 练习/Project/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { getCurrentInstance, onMounted, onErrorCaptured, callWithErrorHandling } from 'vue';
+
+
+
+const instance = getCurrentInstance();
+
+onMounted(() => {
+    console.log(instance);
+})
+
+</script>
+
+<style lang="less" scoped>
+    
+</style>
